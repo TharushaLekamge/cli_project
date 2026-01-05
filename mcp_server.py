1f72c2749660508d7f9a977b171da8f4642c63ec
Tharusha Lekamge <62855616+TharushaLekamge@users.noreply.github.com>
2026-01-05 10:26:24 +0530

    add prompts

--- mcp_server.py
+++ mcp_server.py
@@ -1,4 +1,5 @@
 from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp .prompts import base
 from pydantic import Field
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
@@ -25,7 +26,6 @@ def read_doc_contents(
 
     return docs[doc_name]
 
-# TODO: Write a tool to edit a doc
 
 @mcp.tool(
     name="edit_doc_contents",
@@ -43,7 +43,6 @@ def edit_doc_contents(
     docs[doc_name] = updated_contents
     return updated_contents
 
-# TODO: Write a resource to return all doc id's
 
 @mcp.resource(
     "docs:://documents",
@@ -53,7 +52,7 @@ def edit_doc_contents(
 def list_documents():
     return list(docs.keys())
 
-# TODO: Write a resource to return the contents of a particular doc
+
 @mcp.resource(
     "docs:://documents/{doc_id}",
     description="Return the contents of a particular document given its ID.",
@@ -66,7 +65,47 @@ def get_document(doc_id: str):
     return docs[doc_id]
 
 # TODO: Write a prompt to rewrite a doc in markdown format
-# TODO: Write a prompt to summarize a doc
+@mcp.prompt(
+    name="format",
+    description="Rewrites the contents of the document in Markdown format."
+)
+def format_document(
+        doc_id: str = Field(description="Id of the document to format")
+) -> list[base.Message]:
+    prompt = f"""
+Your goal is to reformat a document to be written with markdown syntax.
+
+The id of the document you need to reformat is:
+<document_id>
+{doc_id}
+</document_id>
+
+Add in headers, bullet points, tables, etc as necessary. Feel free to add in structure.
+Use the 'edit_document' tool to edit the document. After the document has been reformatted...
+"""
+
+    return [
+        base.UserMessage(prompt)
+    ]
+
+
+@mcp.prompt(
+    name="summarize",
+    description="Summarizes the contents of a document."
+)
+def summarize_document(
+        doc_id: str = Field(description="Id of the document to summarize")
+) -> list[base.Message]:
+    prompt = f"""
+Your goal is to summarize the contents of a document.
+The id of the document you need to summarize is:
+<document_id>
+{doc_id}
+</document_id>
+"""
+    return [
+        base.UserMessage(prompt)
+    ]
 
 
 if __name__ == "__main__":
